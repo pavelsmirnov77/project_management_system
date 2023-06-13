4678bbbfb854b66057c759d9e6ec1f3ba56a34f2
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2023-06-14 01:46:24 +0300

    Добавление новой аннотации

--- src/main/java/com/example/project_management_system/configs/SecurityConfig.java
+++ src/main/java/com/example/project_management_system/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.project_management_system.configs;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
