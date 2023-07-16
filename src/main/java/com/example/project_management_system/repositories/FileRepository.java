b6bc22310eb5e23db4ce0cccb305d471e36da16f
Pavel Smirnov <64857615+pavelsmirnov77@users.noreply.github.com>
2023-07-16 18:15:42 +0300

    Delete src directory

--- src/main/java/com/example/project_management_system/repositories/FileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.project_management_system.repositories;
-
-import com.example.project_management_system.entities.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {}
--- src/main/java/com/example/project_management_system/repositories/ImageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.project_management_system.repositories;
-
-import com.example.project_management_system.entities.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {}
