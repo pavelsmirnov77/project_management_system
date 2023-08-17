0ce9bf96aa859d41e0603f766f646d378824f611
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2023-08-17 04:15:02 +0300

    Исправление сущности домашнего задания

--- backend/src/main/java/com/example/backend/entities/Homework.java
+++ backend/src/main/java/com/example/backend/entities/Homework.java
@@ -1,9 +1,12 @@
 package com.example.backend.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "homeworks")
@@ -16,6 +19,14 @@ public class Homework {
     private long id;
     private String name;
     private String description;
+    @Column
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateOfPosting;
+    @Column
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime deadline;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private User author;
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     private Subject subject;
 }
